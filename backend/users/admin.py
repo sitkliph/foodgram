a95d6b08e05f3ce2fbd8a02175bee2de6620ad72
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-02-25 23:39:05 +1000

    Change search fields

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,5 +5,6 @@ from users.models import CustomUser
 UserAdmin.fieldsets += (
     ('Additional info', {'fields': ('avatar',)}),
 )
+UserAdmin.search_fields = ('username', 'email')
 
 admin.site.register(CustomUser, UserAdmin)
