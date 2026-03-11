3a38202484bdf78c39923813ffa2dd9f0e3798b9
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-03-12 02:13:20 +1000

    Update Recipe's filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -19,7 +19,31 @@ class RecipeFilter(django_filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
+    is_favorited = django_filters.NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = django_filters.NumberFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags')
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+
+        if not user.is_authenticated:
+            return queryset.none()
+
+        if value == 1:
+            return queryset.filter(favorites__user=user)
+        return queryset.exclude(favorites__user=user)
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+
+        if not user.is_authenticated:
+            return queryset.none()
+
+        if value == 1:
+            return queryset.filter(shopping_cart__user=user)
+        return queryset.exclude(shopping_cart__user=user)
