8ea29a29fd90d619e7ab145e09319d6caaba4d1e
Sergei Bakin <sergey.bakin2000@gmail.com>
2026-03-13 22:56:07 +1000

    Add Docker configs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,9 +8,3 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('s/<str:code>/', redirect_short_link),
 ]
-
-# Temporary, using for dev! TODO: Delete this before review
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
